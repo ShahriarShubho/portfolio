b3c007c773b453a49d5cebc78d6101bcf0fdf7ad
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-05-15 00:45:18 +0600

    project

--- src/Components/Home/Projects/ProjectCard/ProjectCard.js
+++ src/Components/Home/Projects/ProjectCard/ProjectCard.js
@@ -1,38 +1,38 @@
-import React, { useEffect } from 'react';
-import './ProjectCard.css'
+import React, { useEffect } from "react";
+import "./ProjectCard.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const ProjectCard = ({projects}) => {
+const ProjectCard = ({ projects }) => {
   useEffect(() => {
-    AOS.init({duration : 2000});
-  },[])
-    const {title, image, description, technology, liveSite, clint} = projects
-    console.log(projects)
-    return (
-        <div className="col-md-4 text-dark mb-5" data-aos="zoom-in">
-          <div className="rounded shadow-lg">
-          <h3 className="text-center text-color mb-3">{title}</h3>  
-          <div className="boxes">
-          <img className="img-fluid h-100" src={image} alt={title}/>
-          <div className="project-container">
-            <h5 className="text-center">{title}</h5>
-            <p>{description}</p>
-            <strong>Technology : {technology}</strong>
-              <div className="d-flex justify-content-around">
-              <a href={liveSite} target="_blank" rel="noreferrer">Live Site</a>
-              <a href={clint} target="_blank" rel="noreferrer">Github</a>
-              </div>
+    AOS.init({ duration: 2000 });
+  }, []);
+  const { title, image, description, technology, liveSite, clint } = projects;
+  console.log(projects);
+  return (
+    <div className="col-md-4 text-dark mb-5" data-aos="zoom-in">
+      <h3 className="text-center text-color mb-3">{title}</h3>
+      <div className="boxes rounded shadow-lg">
+        <img className="img-fluid h-100" src={image} alt={title} />
+        <div className="project-container">
+          <h5 className="text-center">{title}</h5>
+          <p>{description}</p>
+          <strong>Technology : {technology}</strong>
+          <div className="d-flex justify-content-around">
+            <a href={liveSite} target="_blank" rel="noreferrer">
+              Live Site
+            </a>
+            <a href={clint} target="_blank" rel="noreferrer">
+              Github
+            </a>
           </div>
-          </div>  
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
-
-
 
 /* <Card>
 <Card.Img className="img-fluid rounded" variant="top" src={image} alt={`${title}`}/>
